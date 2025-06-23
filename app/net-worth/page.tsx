e1ed8e30758a367f9a8de63c9fffad80fda482a4
jpatrick5402 <jpatrick5402@gmail.com>
2025-06-23 08:50:13 -0400

    Adding dynamic paging to header

---
+++ app/net-worth/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Header from "@components/Header";
+import { usePathname } from "next/navigation";
+
+export default function NetWorth() {
+  return (
+    <div>
+      <Header />
+      Net Worth
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,74 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  let pathname = usePathname();
   return (
-    <nav className="flex mt-5 mb-5 bg-stone-500 p-1 rounded-lg items-center">
+    <nav className="flex mt-5 mb-5 bg-stone-500 p-1 rounded-lg items-center shadow-xl/20">
       <Image width="70" height="70" src="/octopus.png" alt="Octopus Logo" />
       <h1 className="text-3xl m-5">Finance Hub</h1>
-      <span className="flex gap-5 ml-10">
-        <Link href="/">Dashboard</Link>
-        <Link href="/">Net Worth</Link>
-        <Link href="/">Budget</Link>
-        <Link href="/">Assets</Link>
-        <Link href="/">Debts</Link>
+      <span className="flex gap-5 ml-10 items-center ">
+        <Link
+          href="/"
+          className={
+            pathname.split("/")[1] == ""
+              ? "rounded-lg p-2 bg-(--color-primary)"
+              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+          }
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/net-worth"
+          className={
+            pathname.split("/")[1] == "net-worth"
+              ? "rounded-lg p-2 bg-(--color-primary)"
+              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+          }
+        >
+          Net Worth
+        </Link>
+        <Link
+          href="/budget"
+          className={
+            pathname.split("/")[1] == "budget"
+              ? "rounded-lg p-2 bg-(--color-primary)"
+              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+          }
+        >
+          Budget
+        </Link>
+        <Link
+          href="/assets"
+          className={
+            pathname.split("/")[1] == "assets"
+              ? "rounded-lg p-2 bg-(--color-primary)"
+              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+          }
+        >
+          Assets
+        </Link>
+        <Link
+          href="/debts"
+          className={
+            pathname.split("/")[1] == "debts"
+              ? "rounded-lg p-2 bg-(--color-primary)"
+              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+          }
+        >
+          Debts
+        </Link>
       </span>
+      <button
+        className="ml-auto mr-5 bg-(--color-primary) p-3 rounded-lg transition-all duration-300 hover:shadow-xl/30"
+        onClick={() => {
+          alert("This is not yet implemented");
+        }}
+      >
+        Sign in
+      </button>
     </nav>
   );
 }
