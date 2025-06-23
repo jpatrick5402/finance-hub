da2c896435dfdfedaeb3e0e402d620f43f834135
jpatrick5402 <jpatrick5402@gmail.com>
2025-06-22 23:05:12 -0400

    Adding header and not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function notFound() {
+  return (
+    <div>
+      <Header />
+      <p>404 - Page not found</p>
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="flex mt-5 mb-5 bg-stone-500 p-5 rounded-lg items-center">
+      <Image width="70" height="70" src="/octopus.png" alt="Octopus Logo" />
+      <h1 className="text-3xl m-5">Finance Hub</h1>
+      <span className="flex gap-5">
+        <Link href="/">Dashboard</Link>
+        <Link href="/">Net Worth</Link>
+        <Link href="/">Budget</Link>
+        <Link href="/">Assets</Link>
+        <Link href="/">Debts</Link>
+      </span>
+    </nav>
+  );
+}
