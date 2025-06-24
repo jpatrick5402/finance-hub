93381f22a0c848326d9357cefc0d8d2098e78542
jpatrick5402 <jpatrick5402@gmail.com>
2025-06-24 11:40:46 -0400

    Fixing link style

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     <nav className="flex mt-3 mb-3 sm:mb-14 bg-(--background-accent) p-1 rounded-lg items-center shadow-xl/20">
       <img src="/octopus.png" alt="Octopus Logo" className="m-3 w-15" />
       <h1 className="text-3xl m-5">Finance Hub</h1>
-      <span className="flex gap-5 ml-10 items-center ">
+      <span className="flex gap-5 items-center ">
         <Link
           href="/"
           className={
