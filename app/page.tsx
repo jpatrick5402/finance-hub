d1066716b1a0d7b650f06e75266b108f6b203fe6
jpatrick5402 <jpatrick5402@gmail.com>
2025-06-23 23:09:31 -0400

    Accurate calculations + Dashboard title style

--- app/page.tsx
+++ app/page.tsx
@@ -10,40 +10,49 @@ export default function Home() {
     (total, expense) => total + expense.value,
     0
   );
+  let assetsTotal = test.assets.reduce(
+    (total, asset) => total + asset.value,
+    0
+  );
+  let debtsTotal = test.debts.reduce((total, debt) => total + debt.value, 0);
 
   return (
     <div>
-      <div className="container">Name: {test.name}</div>
-      <div className="container">Net Worth: {1000 - 200}</div>
+      <div className="container text-xl">Name: {test.name}</div>
+      <div className="container text-xl">
+        Net Worth: {(assetsTotal - debtsTotal).toFixed(2)}
+      </div>
       <div className="container">
-        <p>Budget: ${test.salary}</p>
-        <p>Expenses:</p>
+        <p className="text-xl">
+          Monthly Budget: ${(test.salary / 12).toFixed(2)}
+        </p>
+        <p>Monthly Expenses:</p>
         <ul>
           {test.expenses.map((expense, index) => (
             <li key={index}>
-              {expense.name} | -${expense.value}
+              {expense.name} | -${expense.value.toFixed(2)}
             </li>
           ))}
         </ul>
-        <p>Remaining: ${test.salary - expenseTotal}</p>
+        <p>Remaining: ${(test.salary - expenseTotal).toFixed(2)}</p>
       </div>
       <div className="columns-2 gap-4">
         <div className="container">
-          Assets:
+          <p className="text-xl">Assets:</p>
           <ul>
             {test.assets.map((asset, index) => (
               <li key={index}>
-                {asset.name} | ${asset.value} (APY: {asset.APY})
+                {asset.name} | ${asset.value.toFixed(2)} (APY: {asset.APY})
               </li>
             ))}
           </ul>
         </div>
         <div className="container">
-          Debts:
+          <p className="text-xl">Debts:</p>
           <ul>
             {test.debts.map((debt, index) => (
               <li key={index}>
-                {debt.name} | -${debt.value} (APR: {debt.APR})
+                {debt.name} | -${debt.value.toFixed(2)} (APR: {debt.APR})
               </li>
             ))}
           </ul>
