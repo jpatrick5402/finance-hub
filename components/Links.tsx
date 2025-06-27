add6aad72cd17648d10214295f61e84facaf67bc
jpatrick5402 <jpatrick5402@gmail.com>
2025-06-27 14:49:02 -0400

    Remove fake header sign in button

--- components/Links.tsx
+++ components/Links.tsx
@@ -11,81 +11,57 @@ interface LinksProps {
 
 export default function Links({ current, setCurrent }: LinksProps) {
   return (
-    <>
-      <span className="flex gap-5 mb-3 sm:mb-0 items-center flex-col sm:flex-row">
-        <Link
-          href="/dashboard"
-          className={
-            current == "dashboard"
-              ? "rounded-lg p-2 bg-(--color-primary) shadow-lg"
-              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
-          }
-        >
-          Dashboard
-        </Link>
-        <Link
-          href="/net-worth"
-          className={
-            current == "net-worth"
-              ? "rounded-lg p-2 bg-(--color-primary)"
-              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
-          }
-        >
-          Net Worth
-        </Link>
-        <Link
-          href="/budget"
-          className={
-            current == "budget"
-              ? "rounded-lg p-2 bg-(--color-primary)"
-              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
-          }
-        >
-          Budget
-        </Link>
-        <Link
-          href="/assets"
-          className={
-            current == "assets"
-              ? "rounded-lg p-2 bg-(--color-primary)"
-              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
-          }
-        >
-          Assets
-        </Link>
-        <Link
-          href="/debts"
-          className={
-            current == "debts"
-              ? "rounded-lg p-2 bg-(--color-primary)"
-              : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
-          }
-        >
-          Debts
-        </Link>
-      </span>
-      <div className="ml-auto">
-        <input
-          type="text"
-          id="idInput"
-          className="bg-(--background) m-3 p-3 rounded-lg text-(--foreground)"
-          placeholder="User ID"
-        />
-        <button
-          className="m-3 ml-auto bg-(--color-primary) p-3 rounded-lg transition-all duration-300 hover:shadow-xl/30 hover:bg-(--color-secondary) hover:text-black"
-          onClick={() => {
-            let el = document.getElementById(
-              "idInput"
-            ) as HTMLInputElement | null;
-            if (!el) return;
-            let value = el.value;
-            Cookies.set("userId", value.toString(), { expires: 7 });
-            location.reload();
-          }}
-        >
-          Sign in
-        </button>
-      </div>
-    </>
+    <span className="flex gap-5 mb-3 sm:mb-0 items-center flex-col sm:flex-row">
+      <Link
+        href="/dashboard"
+        className={
+          current == "dashboard"
+            ? "rounded-lg p-2 bg-(--color-primary) shadow-lg"
+            : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+        }
+      >
+        Dashboard
+      </Link>
+      <Link
+        href="/net-worth"
+        className={
+          current == "net-worth"
+            ? "rounded-lg p-2 bg-(--color-primary)"
+            : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+        }
+      >
+        Net Worth
+      </Link>
+      <Link
+        href="/budget"
+        className={
+          current == "budget"
+            ? "rounded-lg p-2 bg-(--color-primary)"
+            : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+        }
+      >
+        Budget
+      </Link>
+      <Link
+        href="/assets"
+        className={
+          current == "assets"
+            ? "rounded-lg p-2 bg-(--color-primary)"
+            : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+        }
+      >
+        Assets
+      </Link>
+      <Link
+        href="/debts"
+        className={
+          current == "debts"
+            ? "rounded-lg p-2 bg-(--color-primary)"
+            : "rounded-lg p-2 hover:bg-(--color-secondary) hover:text-black hover:shadow-lg/20 transition-all duration-300 "
+        }
+      >
+        Debts
+      </Link>
+    </span>
   );
 }
