1f273c3c098f68d265e1a4957ff4fa15d44f51d1
jpatrick5402 <jpatrick5402@gmail.com>
2025-06-30 15:43:22 -0400

    because I'm bad at cybersecurity

--- lib/save.ts
+++ lib/save.ts
@@ -4,7 +4,6 @@ export async function save(formValues: User, currentUser: User) {
   if (formValues.full_name == "") formValues.full_name = currentUser.full_name;
   if (formValues.salary == 0) formValues.salary = currentUser.salary;
 
-  console.log(formValues);
   let response = await fetch("/api/user/set", {
     method: "POST",
     body: JSON.stringify(formValues),
