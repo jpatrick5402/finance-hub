9feaffad2bf15c051f197cd8e0a0b92d4a3a4264
jpatrick5402 <jpatrick5402@gmail.com>
2025-07-18 13:36:05 -0400

    style

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,48 +54,6 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto`}
       >
         <SessionProvider session={session} refetchOnWindowFocus={false}>
-          {/* Auth */}
-          {!session?.user ? (
-            <div className="container grid gap-3">
-              <p>Please sign in to save your data :)</p>
-              <form
-                className="m-auto"
-                action={async (formData) => {
-                  "use server";
-                  const email = formData.get("email")?.toString();
-                  if (email && emailSchema.safeParse(email).success) {
-                    await signIn("resend", formData);
-                  }
-                }}
-              >
-                <input
-                  className="border-b-2 border-b-(--color-primary) m-2"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                />
-                <button className="btn" type="submit">
-                  Sign in with your email
-                </button>
-              </form>
-              <div className="flex flex-col sm:flex-row m-auto self-center">
-                {authProviders.map((provider) => {
-                  return (
-                    <button
-                      key={provider}
-                      className="btn m-3"
-                      onClick={async () => {
-                        "use server";
-                        await login(provider, "/dashboard");
-                      }}
-                    >
-                      Sign in with {provider}
-                    </button>
-                  );
-                })}
-              </div>
-            </div>
-          ) : null}
           <nav className="flex mt-3 mb-3 bg-(--color-header) p-1 rounded-lg items-center shadow-xl/20">
             <a href="/">
               <Image
@@ -123,6 +81,61 @@ export default async function RootLayout({
             )}
             <Links links={links} />
           </nav>
+          {/* Auth */}
+          {!session?.user ? (
+            <div className="m-auto bg-(--color-container) grid gap-3 rounded-lg p-5 backdrop-blur-2xl shadow-2xl shadow-[#00000085]">
+              <p className="m-auto text-xl">
+                Please sign in to save your data :)
+              </p>
+              <div className="flex flex-col sm:flex-row m-auto self-center">
+                <form
+                  className="flex flex-col sm:flex-row"
+                  action={async (formData) => {
+                    "use server";
+                    const email = formData.get("email")?.toString();
+                    if (email && emailSchema.safeParse(email).success) {
+                      await signIn("resend", formData);
+                    }
+                  }}
+                >
+                  <input
+                    className="border-b-2 border-b-(--color-primary) m-2"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+                  <button className="btn" type="submit">
+                    Sign in with your email
+                  </button>
+                </form>
+              </div>
+              <p className="m-auto">- or -</p>
+              <div className="flex flex-col sm:flex-row m-auto self-center">
+                {authProviders.map((provider) => {
+                  return (
+                    <button
+                      key={provider}
+                      className="btn m-3"
+                      onClick={async () => {
+                        "use server";
+                        await login(provider, "/dashboard");
+                      }}
+                    >
+                      <div className="flex flex-row gap-2">
+                        <img
+                          height={20}
+                          width={30}
+                          src={"/" + provider + ".png"}
+                          alt={provider}
+                        />
+                        <p className="m-auto">Sign in with {provider}</p>
+                      </div>
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          ) : null}
           <div className="grow-1">{children}</div>
           <footer className="flex justify-center items-center p-4 bg-(--color-footer) mt-8 rounded-t-lg">
             <p className="text-sm mr-4 ml-4">
