e2e7e02c11a00c3719dccd0026abd8c55b4ffc7d
jpatrick5402 <jpatrick5402@gmail.com>
2025-07-21 20:44:48 -0400

    Better info icon behavior

--- app/components/InfoIcon.tsx
+++ app/components/InfoIcon.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function ({ infoText }: { infoText: string }) {
+  const [showText, setShowText] = useState(false);
+
   return (
-    <img
-      src="/info.png"
-      alt="info"
-      className="h-4 w-4 mt-auto mb-auto mr-1 hover:bg-gray-400 hover:rounded-3xl transition-all duration-700"
-      title={infoText}
-    />
+    <div
+      className="mt-auto mb-auto mr-1 hover:bg-gray-400 hover:rounded-3xl transition-all duration-300"
+      onTouchStart={() => setTimeout(() => setShowText((prev) => !prev), 300)}
+      onMouseOver={() => setShowText((prev) => !prev)}
+      onMouseOut={() => setShowText((prev) => !prev)}
+      style={{ cursor: "pointer" }}
+    >
+      <img src="/info.png" alt="info" className="h-4 w-4 " />
+      {showText && (
+        <p className="text-sm absolute bg-(--color-primary) p-2 rounded mt-3 transition-all duration-300">
+          {infoText}
+        </p>
+      )}
+    </div>
   );
 }
