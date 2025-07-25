20f723c5b039e51c5287df4a5337dd41218105a3
jpatrick5402 <jpatrick5402@gmail.com>
2025-07-25 15:50:14 -0400

    shortening width of banner

--- app/page.tsx
+++ app/page.tsx
@@ -11,52 +11,56 @@ export default function Home() {
     <div>
       {/* Auth */}
       {!session?.user ? (
-        <div className="m-auto bg-(--color-container) grid gap-3 rounded-lg p-5 backdrop-blur-2xl shadow-2xl shadow-[#00000085]">
-          <p className="m-auto text-xl">Please sign in to save your data :)</p>
-          <div className="flex flex-col sm:flex-row m-auto self-center">
-            {authProviders.map((provider) => {
-              return (
-                <button
-                  key={provider}
-                  className="btn m-3"
-                  onClick={async () => {
-                    await login(provider, "/dashboard");
-                  }}
-                >
-                  <div className="flex flex-row gap-2">
-                    <img
-                      height={20}
-                      width={30}
-                      src={"/" + provider + ".png"}
-                      alt={provider}
-                    />
-                    <p className="m-auto">Sign in with {provider}</p>
-                  </div>
+        <div className="flex">
+          <div className="m-auto bg-(--color-container) grid gap-3 rounded-lg p-5 backdrop-blur-2xl shadow-2xl shadow-[#00000085]">
+            <p className="m-auto text-xl">
+              Please sign in to save your data :)
+            </p>
+            <div className="flex flex-col sm:flex-row m-auto self-center">
+              {authProviders.map((provider) => {
+                return (
+                  <button
+                    key={provider}
+                    className="btn m-3"
+                    onClick={async () => {
+                      await login(provider, "/dashboard");
+                    }}
+                  >
+                    <div className="flex flex-row gap-2">
+                      <img
+                        height={20}
+                        width={30}
+                        src={"/" + provider + ".png"}
+                        alt={provider}
+                      />
+                      <p className="m-auto">Sign in with {provider}</p>
+                    </div>
+                  </button>
+                );
+              })}
+            </div>
+            <p className="m-auto">- or -</p>
+            <div className="flex flex-col sm:flex-row m-auto self-center">
+              <form
+                className="flex flex-col sm:flex-row"
+                action={async (formData) => {
+                  const email = formData.get("email")?.toString();
+                  if (email && /^.+\@.+\..+/.test(email)) {
+                    await login("resend", "/dashboard", email);
+                  }
+                }}
+              >
+                <input
+                  className="border-b-2 border-b-(--color-primary) m-2"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+                <button className="btn" type="submit">
+                  Sign in with your email
                 </button>
-              );
-            })}
-          </div>
-          <p className="m-auto">- or -</p>
-          <div className="flex flex-col sm:flex-row m-auto self-center">
-            <form
-              className="flex flex-col sm:flex-row"
-              action={async (formData) => {
-                const email = formData.get("email")?.toString();
-                if (email && /^.+\@.+\..+/.test(email)) {
-                  await login("resend", "/dashboard", email);
-                }
-              }}
-            >
-              <input
-                className="border-b-2 border-b-(--color-primary) m-2"
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              <button className="btn" type="submit">
-                Sign in with your email
-              </button>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       ) : null}
