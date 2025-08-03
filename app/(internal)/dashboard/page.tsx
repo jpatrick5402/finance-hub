abd8332a5cd9cf8a47d285d7327a9c1ed79260d4
jpatrick5402 <jpatrick5402@gmail.com>
2025-08-03 09:13:01 -0400

    Adding basic dashboard

--- app/(internal)/dashboard/page.tsx
+++ app/(internal)/dashboard/page.tsx
@@ -1,13 +1,78 @@
 "use client";
 import { useContext } from "react";
-import { ArcElement, Chart, Tooltip, Title } from "chart.js";
-
+import {
+  Chart,
+  Tooltip,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeSeriesScale,
+  ArcElement,
+} from "chart.js";
+import "chartjs-adapter-date-fns";
 import UserContext from "@contexts/UserContext";
+import { Doughnut, Line } from "react-chartjs-2";
+import {
+  getAssetChartInfo,
+  getBudgetChartInfo,
+  getNetWorthChartInfo,
+  getDebtChartInfo,
+} from "@lib/chartData";
 
-Chart.register(ArcElement, Tooltip, Title);
+Chart.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeSeriesScale,
+  Tooltip
+);
 
 export default function Dashboard() {
   const [user, setUser] = useContext(UserContext);
 
-  return <></>;
+  const [netWorthGraphData, netWorthOptions]: any = getNetWorthChartInfo(user);
+  const budgetGraphData: any = getBudgetChartInfo(user);
+  const debtData = getDebtChartInfo(user);
+  const [
+    fixedAssetsData,
+    investedAssetsData,
+    totalAssetsData,
+    fixedOptions,
+    investedOptions,
+    totalOptions,
+  ]: any = getAssetChartInfo(user);
+
+  return (
+    <div>
+      <div className="flex container flex-col sm:flex-row">
+        <div className="m-auto">
+          <p>Net Worth</p>
+          <Line data={netWorthGraphData} options={netWorthOptions} />
+        </div>
+        <div className="m-auto">
+          <p>Expenses</p>
+          <Doughnut data={budgetGraphData} className="flex m-auto w-50%" />
+        </div>
+        <div className="m-auto">
+          <p>Debts</p>
+          <Doughnut data={debtData} />
+        </div>
+      </div>
+      <div className="flex container flex-col sm:flex-row">
+        <p>Assets</p>
+        <div className="m-auto">
+          <Doughnut data={fixedAssetsData} options={fixedOptions} />
+        </div>
+        <div className="m-auto">
+          <Doughnut data={investedAssetsData} options={investedOptions} />
+        </div>
+        <div className="m-auto">
+          <Doughnut data={totalAssetsData} options={totalOptions} />
+        </div>
+      </div>
+    </div>
+  );
 }
